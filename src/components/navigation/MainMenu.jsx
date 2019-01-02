ca53fcd64686c324605dcc517045efa24a96cb13
Thomas Alisi <thomasalisi@gmail.com>
2019-01-02 13:12:45 +0100

    added material-ui

--- src/components/navigation/MainMenu.jsx
+++ src/components/navigation/MainMenu.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
 
 import Authentication from './Authentication';
 
 const MainMenu = () => (
-  <nav>
-    <ul>
-      <li><Authentication /></li>
-      <li><Link to="/">What is DevCon.London</Link></li>
-      <li><Link to="/terms">Terms</Link></li>
-      {/* shown when not authenticated or authenticated and not a member */}
-      <li><Link to="/subscribe">Subscribe</Link></li>
-      {/* shown when authenticated and member */}
-      <li><Link to="/events">Events</Link></li>
-      <li><Link to="/members">Members</Link></li>
-      {/* shown when authenticated, member and admin */}
-      <li><Link to="/submissions">Submissions</Link></li>
-    </ul>
-  </nav>
+  <AppBar>
+    <ToolBar>
+      <IconButton aria-label="Open drawer" color="inherit">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" color="inherit">
+        <Link to="/">DevCon.London</Link>
+      </Typography>
+      <Button><Link to="/terms">Terms</Link></Button>
+      <Button><Link to="/subscribe">Subscribe</Link></Button>
+      <Button><Link to="/events">Events</Link></Button>
+      <Button><Link to="/members">Members</Link></Button>
+      <Button><Link to="/submissions">Submissions</Link></Button>
+      <Authentication />
+    </ToolBar>
+  </AppBar>
 );
 
 export default MainMenu;
