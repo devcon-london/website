6af49d9fd08f187fa6c849fe23ac4196d2e4286e
Silvio Paganini <silvio@fluuu.id>
2019-11-27 11:25:42 +0000

    adding automated invite to approved user to slack

--- functions/slack.js
+++ functions/slack.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const functions = require('firebase-functions');
 const { IncomingWebhook } = require('@slack/webhook');
 
@@ -16,3 +17,17 @@ exports.notifySlack = (msg) => {
       console.log('error while posting to slack', e);
     });
 };
+
+exports.inviteSlack = async (email) => {
+  try {
+    const SLACK_TOKEN = functions.config().slack_integration.token;
+    const SLACK_INVITE_ENDPOINT = 'https://slack.com/api/users.admin.invite';
+    const QUERY_PARAMS = `email=${email}&token=${SLACK_TOKEN}&set_active=true`;
+    
+    const request = await axios.get(`${SLACK_INVITE_ENDPOINT}?${QUERY_PARAMS}`)
+    // TODO: return what you need after the request
+    console.log(request)
+  } catch (e) {
+    throw new Error(e)
+  }
+}
